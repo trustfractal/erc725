b1f8da34942e5b2429117fcaef2dce0e00a20037
Júlio Santos <hi@whoisjuliosantos.com>
2018-06-20 18:05:30 +0200

    Change ERC725 standard to make execute method payable

--- contracts/KeyHolder.sol
+++ contracts/KeyHolder.sol
@@ -83,7 +83,7 @@ contract KeyHolder is ERC725 {
 
         if (_approve == true) {
             executions[_id].approved = true;
-            success = executions[_id].to.call(executions[_id].data, 0);
+            success = executions[_id].to.call.value(executions[_id].value)(executions[_id].data, 0);
             if (success) {
                 executions[_id].executed = true;
                 emit Executed(
@@ -110,6 +110,7 @@ contract KeyHolder is ERC725 {
 
     function execute(address _to, uint256 _value, bytes _data)
         public
+        payable
         returns (uint256 executionId)
     {
         require(!executions[executionNonce].executed, "Already executed");
