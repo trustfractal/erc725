926ccf37506d1cb2cdba0bb028b13726d9dfdd96
Júlio Santos <hi@whoisjuliosantos.com>
2018-06-21 10:56:39 +0200

    Refactor and simplify

---
+++ src/contracts.js
@@ -0,0 +1,79 @@
+import "babel-polyfill";
+
+import solc from "solc";
+import fs from "fs";
+
+
+var web3;
+
+var findImportsPath = function(prefix) {
+  return function findImports(path) {
+      return {
+        contents: fs.readFileSync(prefix + path).toString()
+      }
+  }
+};
+
+var compile = (filenames, importsPath) => {
+  var compileOptions = JSON.stringify({
+    sources: filenames.reduce((acc, filename) => {
+      acc[filename] = {
+        content: fs.readFileSync("contracts/" + filename, "utf8").toString()
+      }
+      return acc;
+    }, {}),
+    language: "Solidity",
+    settings: {
+      metadata: {useLiteralContent: true},
+      outputSelection: {
+        "*": {
+          "*": ["abi", "evm.bytecode.object"]
+        }
+      }
+    }
+  });
+
+  var compileOutput = solc.compileStandardWrapper(
+    compileOptions,
+    findImportsPath(importsPath)
+  );
+
+  var jsonOutput = JSON.parse(compileOutput);
+
+  // console.log(jsonOutput.errors);
+
+  return jsonOutput;
+};
+
+module.exports = {
+  init: (_web3) => (web3 = _web3),
+  compileCrowdsale: () => (
+    compile([
+      "ERC725.sol",
+      "ERC735.sol",
+      "KeyHolder.sol",
+      "ClaimHolder.sol",
+      "VeryGoodCoin.sol",
+      "VeryGoodCrowdsale.sol",
+    ], "node_modules/")
+  ),
+  compileClaimHolder: () => (
+    compile([
+      "ClaimHolder.sol",
+    ], "contracts/")
+  ),
+  deploy: async (contractName, jsonCompileOutput, from, constructorArgs) => {
+    var {abi, evm: {bytecode: {object: bytecode}}} = jsonCompileOutput.contracts[contractName + ".sol"][contractName];
+
+    var Contract = new web3.eth.Contract(abi);
+    var contract = await Contract.deploy({
+      data: bytecode,
+      arguments: constructorArgs,
+    }).send({
+      from,
+      gas: 3000000,
+    });
+
+    return contract;
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import "babel-polyfill";
 
 import Web3 from 'web3';
-import ganache from "ganache-cli";
-import solc from "solc";
-import fs from "fs";
+
+import Contracts from "./contracts";
+
+
+console.log("\r");
 
 const KEY_PURPOSES = {
   "MANAGEMENT" : 1,
@@ -19,163 +21,39 @@ const CLAIM_TYPES = {
   "KYC" : 7
 };
 
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-var accounts;
-
-var compileClaimHolder = () => {
-  var findImportsPath = function(prefix) {
-    return function findImports(path) {
-        return {
-          contents: fs.readFileSync(prefix + path).toString()
-        }
-    }
-  }
-
-  var compileOptions = JSON.stringify({
-    sources: {
-      "ClaimHolder.sol": {
-        content: fs.readFileSync("contracts/ClaimHolder.sol", "utf8").toString()
-      },
-    },
-    language: 'Solidity',
-    settings: {
-      metadata: { useLiteralContent: true },
-      outputSelection: {
-        '*': {
-          '*': ['abi', 'evm.bytecode.object']
-        }
-      }
-    }
-  });
-
-  var compileOutput = solc.compileStandardWrapper(
-    compileOptions,
-    findImportsPath("contracts/")
-  );
-
-  var jsonOutput = JSON.parse(compileOutput);
-
-  console.log(jsonOutput.errors);
-
-  return jsonOutput;
-};
-
-var compileCrowdsale = () => {
-  var findImportsPath = function(prefix) {
-    return function findImports(path) {
-        return {
-          contents: fs.readFileSync(prefix + path).toString()
-        }
-    }
-  }
-
-  var compileOptions = JSON.stringify({
-    sources: {
-      "ERC725.sol": {
-        content: fs.readFileSync("contracts/ERC725.sol", "utf8").toString()
-      },
-      "ERC735.sol": {
-        content: fs.readFileSync("contracts/ERC735.sol", "utf8").toString()
-      },
-      "KeyHolder.sol": {
-        content: fs.readFileSync("contracts/KeyHolder.sol", "utf8").toString()
-      },
-      "ClaimHolder.sol": {
-        content: fs.readFileSync("contracts/ClaimHolder.sol", "utf8").toString()
-      },
-      "VeryGoodCoin.sol": {
-        content: fs.readFileSync("contracts/VeryGoodCoin.sol", "utf8").toString()
-      },
-      "VeryGoodCrowdsale.sol": {
-        content: fs.readFileSync("contracts/VeryGoodCrowdsale.sol", "utf8").toString()
-      },
-    },
-    language: 'Solidity',
-    settings: {
-      metadata: { useLiteralContent: true },
-      outputSelection: {
-        '*': {
-          '*': ['abi', 'evm.bytecode.object']
-        }
-      }
-    }
-  });
-
-  var compileOutput = solc.compileStandardWrapper(
-    compileOptions,
-    findImportsPath("node_modules/")
-  );
-
-  var jsonOutput = JSON.parse(compileOutput);
-
-  console.log(jsonOutput.errors);
-
-  return jsonOutput;
-};
-
-var deployClaimHolder = async (jsonClaimHolder, account) => {
-  var abi = jsonClaimHolder.contracts["ClaimHolder.sol"].ClaimHolder.abi;
-  var bytecode = jsonClaimHolder.contracts["ClaimHolder.sol"].ClaimHolder.evm.bytecode.object;
-
-  var ClaimHolder = new web3.eth.Contract(abi);
-  var claimHolder = await ClaimHolder.deploy({
-    data: bytecode
-  }).send({
-    from: account,
-    gas: 3000000,
-  });
-
-  return claimHolder;
-};
-
-var deployVeryGoodCoin = async (jsonClaimHolder, account) => {
-  var abi = jsonClaimHolder.contracts["VeryGoodCoin.sol"].VeryGoodCoin.abi;
-  var bytecode = jsonClaimHolder.contracts["VeryGoodCoin.sol"].VeryGoodCoin.evm.bytecode.object;
-
-  var ClaimHolder = new web3.eth.Contract(abi);
-  var claimHolder = await ClaimHolder.deploy({
-    data: bytecode,
-  }).send({
-    from: account,
-    gas: 3000000,
-  });
-
-  return claimHolder;
-};
-
-var deployVeryGoodCrowdsale = async (jsonClaimHolder, account, constructorArguments) => {
-  var abi = jsonClaimHolder.contracts["VeryGoodCrowdsale.sol"].VeryGoodCrowdsale.abi;
-  var bytecode = jsonClaimHolder.contracts["VeryGoodCrowdsale.sol"].VeryGoodCrowdsale.evm.bytecode.object;
-
-  var ClaimHolder = new web3.eth.Contract(abi);
-  var claimHolder = await ClaimHolder.deploy({
-    data: bytecode,
-    arguments: constructorArguments,
-  }).send({
-    from: account,
-    gas: 3000000,
-  });
-
-  return claimHolder;
-};
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+Contracts.init(web3);
 
 (async () => {
-  accounts = await web3.eth.personal.getAccounts();
+  /*
+   * compiling contracts
+   */
+
+  var accounts = await web3.eth.personal.getAccounts();
 
   console.log("Compiling ClaimHolder...");
 
-  var jsonClaimHolder = compileClaimHolder();
-  var jsonCrowdsale = compileCrowdsale();
+  var jsonClaimHolder = Contracts.compileClaimHolder();
+  var jsonCrowdsale = Contracts.compileCrowdsale();
 
-  // fractalId deploys claim holder
+
+  /*
+   * FractalId deploys own ClaimHolder contract
+   */
+
   console.log("Deploying Fractal's ClaimHolder...");
 
-  var fractalIdClaimHolder = await deployClaimHolder(
+  var fractalIdClaimHolder = await Contracts.deploy(
+    "ClaimHolder",
     jsonClaimHolder,
-    accounts[0]
+    accounts[0],
   );
 
-  // fractalId adds claim key
+
+  /*
+   * FractalId adds claim key on its ClaimHolder
+   */
+
   console.log("Adding a claim key to Fractal's ClaimHolder...");
 
   var fractalIdClaimKey = web3.utils.keccak256(accounts[1]);
@@ -188,15 +66,24 @@ var deployVeryGoodCrowdsale = async (jsonClaimHolder, account, constructorArgume
     gas: 4612388,
   });
 
-  // investor deploys claim holder
+
+  /*
+   * Investor deploys their ClaimHolder contract
+   */
+
   console.log("Deploying Investor's ClaimHolder...");
 
-  var investorClaimHolder = await deployClaimHolder(
+  var investorClaimHolder = await Contracts.deploy(
+    "ClaimHolder",
     jsonClaimHolder,
     accounts[2],
   );
 
-  // fractalId signs a KYC claim
+
+  /*
+   * FractalId signs a KYC claim for Investor to add to their ClaimHolder
+   */
+
   console.log("Signing FractalId's KYC claim...");
 
   var hexedData = web3.utils.asciiToHex("Yea no, this guy is totes legit");
@@ -207,7 +94,11 @@ var deployVeryGoodCrowdsale = async (jsonClaimHolder, account, constructorArgume
   );
   var signature = await web3.eth.sign(hashedDataToSign, accounts[0]);
 
-  // Investor adds FractalId's claim to Investor's ClaimHolder
+
+  /*
+   * Investor adds FractalId's claim to own ClaimHolder
+   */
+
   console.log("Adding FractalId's KYC claim on Investor's ClaimHolder...");
 
   var claimIssuer = fractalIdClaimHolder.options.address;
@@ -236,50 +127,66 @@ var deployVeryGoodCrowdsale = async (jsonClaimHolder, account, constructorArgume
   // FIXME why doesn't this work?
   //var fractalIdKYCclaimId = web3.utils.keccak256(claimIssuer, CLAIM_TYPES.KYC);
   //console.log(await investorClaimHolder.methods.getClaim(fractalIdKYCclaimId).call());
-  // XXX but this does?
+  // FIXME but this does?
   //console.log("Claim", await investorClaimHolder.methods.getClaimIdsByType(CLAIM_TYPES.KYC).call());
 
 
-  // FractalLp deploys token and crowdsale
+  /*
+   * FractalLp deploys token and crowdsale
+   */
 
   console.log("FractalLp deploying token and crowdsale contracts...");
-  var veryGoodCoin = await deployVeryGoodCoin(
+  var veryGoodCoin = await Contracts.deploy(
+    "VeryGoodCoin",
     jsonCrowdsale,
     accounts[0],
   );
 
-  var veryGoodCrowdsale = await deployVeryGoodCrowdsale(
+  var veryGoodCrowdsale = await Contracts.deploy(
+    "VeryGoodCrowdsale",
     jsonCrowdsale,
     accounts[0],
-    [10, accounts[0], veryGoodCoin.options.address, fractalIdClaimHolder.options.address]
+    [10, accounts[0], veryGoodCoin.options.address, fractalIdClaimHolder.options.address],
   );
 
-  await veryGoodCoin.methods.transferOwnership(veryGoodCrowdsale.options.address).send({from: accounts[0]});
+  await veryGoodCoin.methods.transferOwnership(
+    veryGoodCrowdsale.options.address
+  ).send({
+    from: accounts[0]
+  });
 
 
-  // Investor makes transfer
+  /*
+   * Investor makes transfer
+   */
   
   console.log("Investor participating in crowdsale, which will check for the claim...");
-  console.log("\tInvestor initial balance:", await veryGoodCoin.methods.balanceOf(accounts[2]).call());
+  console.log(
+    "\t",
+    "Investor initial balance:",
+    await veryGoodCoin.methods.balanceOf(accounts[2]).call()
+  );
 
   var investABI = await veryGoodCrowdsale.methods.buyTokens(
     investorClaimHolder.options.address
   ).encodeABI();
 
-  // FIXME the reason this isn't working is because there's no eth being sent
+  var investmentAmount = web3.utils.toWei("1", "ether");
   await investorClaimHolder.methods.execute(
     veryGoodCrowdsale.options.address,
-    web3.utils.toWei("1", "ether"),
+    investmentAmount,
     investABI,
   ).send({
     gas: 4612388,
     from: accounts[2],
-    value: web3.utils.toWei("1", "ether"),
+    value: investmentAmount,
   });
 
-  //console.log(await veryGoodCrowdsale.getPastEvents("allEvents", {fromBlock: 0, toBlock: "latest"}));
-  console.log("\tInvestor's subsequent balance:", await veryGoodCoin.methods.balanceOf(investorClaimHolder.options.address).call());
-  //console.log(await veryGoodCoin.methods.totalSupply().call());
-  //console.log(await veryGoodCrowdsale.methods.weiRaised().call());
-
+  console.log(
+    "\t",
+    "Investor's subsequent balance:",
+    await veryGoodCoin.methods.balanceOf(
+      investorClaimHolder.options.address
+    ).call()
+  );
 })()
